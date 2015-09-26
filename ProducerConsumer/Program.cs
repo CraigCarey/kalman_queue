ef40eed2c109d167a29959daff7071d83751a285
Craig Carey <sgccarey@gmail.com>
2015-09-26 18:36:31 +0100

    Basic queue working using BlockinCollection

--- ProducerConsumer/Program.cs
+++ ProducerConsumer/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProducerConsumer
@@ -10,6 +11,31 @@ namespace ProducerConsumer
     {
         static void Main(string[] args)
         {
+            int numTasks = 100;
+
+            var taskQueue = new BlockingCollection<string>();
+            
+            Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < numTasks; i++)
+                {
+                    Console.WriteLine("Producer\tTask: " + i);
+                    taskQueue.Add("task: " + i);
+                    Thread.Sleep(100);
+                }
+            });
+
+            Task.Factory.StartNew(() =>
+            {
+                foreach (string value in taskQueue.GetConsumingEnumerable())
+                {
+                    Console.WriteLine("Consumer\t Task: " + value);
+                    Thread.Sleep(150);
+                }   
+            });
+
+            Task.WaitAll();
+            Console.ReadLine();
         }
     }
 }
